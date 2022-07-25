34c34dd56ecba5372415d6f9724d1c44bb3e79af
Angel Sanabria <106607169+asanabria-2021067@users.noreply.github.com>
2022-07-24 22:19:04 -0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#btn");
+let sideban = document.querySelector(".sideban");
+let searchBtn = document.querySelector(".bx-search");
+
+btn.onclick = function () {
+  sideban.classList.toggle("active");
+};
+
